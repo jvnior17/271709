79f2762905943eb363801e8341d9b4564f96f5ae
jvnior17 <93047220+jvnior17@users.noreply.github.com>
2022-02-02 11:34:27 -0500

    updatingdb

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const db = knex({
   // connect to your own database here:
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-polished-99810',
     user : 'paulamponsah',
     password : '',
     database : 'smartbrain'
